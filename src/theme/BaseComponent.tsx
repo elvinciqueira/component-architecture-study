4b8902ac024d0107684919b84163c7ba7c003479
Elvin Ciqueira <elvinciqueira@gmail.com>
2022-09-26 18:47:26 -0300

    style: add default style

--- src/theme/BaseComponent.tsx
+++ src/theme/BaseComponent.tsx
@@ -1,16 +1,29 @@
-import { parseStyleSheet } from "@skynexui/responsive_stylesheet";
+import React from "react";
 import styled from "styled-components";
-import { StyleSheet } from "./StyleSheet";
+import { StyleSheet } from "@src/theme/StyleSheet";
+import { parseStyleSheet } from "@skynexui/responsive_stylesheet";
 
-interface StyledBaseComponentProps {
+interface StyledBaseComponent {
   styleSheet?: StyleSheet;
+  ref: any;
 }
 
-export const BaseComponent = (props) => {
-  return <StyledBaseComponent {...props} />;
-};
+interface BaseComponentProps {
+  styleSheet: StyleSheet;
+  [key: string]: any;
+}
 
-const StyledBaseComponent = styled.div<StyledBaseComponentProps>`
+export const BaseComponent = React.forwardRef<unknown, BaseComponentProps>(
+  (props, ref) => {
+    return <StyledBaseComponent ref={ref} {...props} />;
+  }
+);
+
+const StyledBaseComponent = styled.div<StyledBaseComponent>`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  flex-shrink: 0;
   ${({ styleSheet }) => parseStyleSheet(styleSheet)}
 `;
 
